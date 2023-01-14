b8858e9e37f99156ff010e98cbc51025b580c492
Neelabh Singh <neelabh041997@gmail.com>
2023-01-14 18:36:44 +0530

    Existing User Login checks

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js"
 import { register } from "./controllers/auth.js";
 
 /* Configurations */ 
@@ -37,7 +38,10 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 /* Routes with Files*/
-app.post("/auth/register", upload.single("picture"), register); 
+app.post("/auth/register", upload.single("picture"), register);
+
+/* Routes */
+app.use("/auth", authRoutes);
 
 /* Mongoose Setup */
 const PORT = process.env.PORT || 6001;
