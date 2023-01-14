61c9f2f1678257b242e787179d4e4b4c86382288
Neelabh Singh <neelabh041997@gmail.com>
2023-01-14 17:25:58 +0530

    User schema, Register User and Assets folder

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
 
 /* Configurations */ 
 const __filename = fileURLToPath(import.meta.url);
@@ -34,6 +35,9 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+/* Routes with Files*/
+app.post("/auth/register", upload.single("picture"), register); 
 
 /* Mongoose Setup */
 const PORT = process.env.PORT || 6001;
